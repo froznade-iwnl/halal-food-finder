86b0ef17050219e51574d0893e86b2853e9e572e
Raditya <raditya.learner@gmail.com>
2022-11-05 09:25:23 +0800

    Initial Commit

---
+++ HalalFoodFinder/HalalFoodFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalalFoodFinderApp.swift
+//  HalalFoodFinder
+//
+//  Created by CTSS Students on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct HalalFoodFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
